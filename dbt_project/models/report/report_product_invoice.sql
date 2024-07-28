13cf022db8c856da3180ea7e49ed2d4d7a3974c2
saksit chobnagn <saksit.ch24@gmail.com>
2024-07-28 22:46:01 +0700

    Update report_product_invoice.sql

--- dbt_project/models/report/report_product_invoice.sql
+++ dbt_project/models/report/report_product_invoice.sql
@@ -5,7 +5,7 @@ SELECT
   dim_product.description,
   SUM(fct_invoices.quantity) AS total_quantity_sold
 FROM {{ ref('fct_invoices') }} fct_invoices
-JOIN {{ ref('dim_product') }} dim_product ON fct_invoices.product_id = p.product_id
+JOIN {{ ref('dim_product') }} dim_product ON fct_invoices.product_id = dim_product.product_id
 GROUP BY dim_product.product_id, dim_product.stock_code, dim_product.description
 ORDER BY total_quantity_sold DESC
 LIMIT 10
